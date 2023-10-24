1a6c041e993912f17450d7200b145ea4add2147d
Akirch19 <austin.kirchner76@gmail.com>
2023-10-24 14:12:19 -0700

    all foundations done

--- final/Foundation4/Program.cs
+++ final/Foundation4/Program.cs
@@ -4,6 +4,21 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation4 World!");
+        List<Activity> activitiesList = new List<Activity>();
+
+        Running running = new Running(40, 4);
+        activitiesList.Add(running);
+        Cycling cycling = new Cycling(90, 30);
+        activitiesList.Add(cycling);
+        Swimming swimming = new Swimming(40, 20);
+        activitiesList.Add(swimming);
+
+
+        //summaries
+        foreach (Activity activity in activitiesList)
+        {
+            activity.DisplaySummary();
+        }
+
     }
 }
